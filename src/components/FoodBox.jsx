fcba1112425c425240949f1c9254348a0032f57a
Alberto Gómez <albertogomrod5@gmail.com>
2023-03-01 22:06:08 +0100

    Intento de botón delete

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -2,20 +2,36 @@ import { Card, Col, Button } from 'antd';
 
 // Iteration 2
 function FoodBox(props) {
-  const removeFood = (objetoDelete)=>{
-    
-  }
-  
+  const removeFood = (comidaBorrada) => {
+    const removeFood = props.filterFood.filter((cadaComida) => {
+      if (cadaComida === comidaBorrada) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    props.filterFood(removeFood);
+  };
+
   return (
     <Col>
-      <Card title={props.food.name} style={{ width: 230, height: 300, margin: 10 }}>
+      <Card
+        title={props.food.name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
         <img src={props.food.image} height={60} alt="food" />
         <p>Calories: {props.food.calories}</p>
         <p>Servings: {props.food.servings}</p>
         <p>
-          <b>Total Calories:{props.food.calories} * {props.food.servings} </b> kcal
+          <b>
+            Total Calories:{props.food.calories} * {props.food.servings}{' '}
+          </b>{' '}
+          kcal
         </p>
-        <Button type="primary" onClick={()=> removeFood(props.food)}> Delete </Button>
+        <Button type="primary" onClick={() => removeFood(props.food)}>
+          {' '}
+          Delete{' '}
+        </Button>
       </Card>
     </Col>
   );
